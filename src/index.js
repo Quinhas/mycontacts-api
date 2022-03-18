6a656972aecfb91bc632a2aa9bca147cbd5fdff5
Quinhas <ln.santana08@gmail.com>
2022-03-18 13:37:02 -0300

    Criando o middleware do CORS na API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 require('express-async-errors');
 
 const routes = require('./routes');
@@ -6,12 +8,11 @@ const routes = require('./routes');
 const PORT = 3000;
 
 const app = express();
+
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-app.use((error, request, response, next) => {
-  console.log('##### Error Handler #####');
-  console.log(error);
-  response.sendStatus(500);
-});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`[server] Server started at http://localhost:${PORT}`));
