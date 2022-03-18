610c18266086dc8b9f62f9a50efa4f3c51db5942
Quinhas <ln.santana08@gmail.com>
2022-03-18 13:27:22 -0300

    Capturando e tratando erros

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -7,5 +8,10 @@ const PORT = 3000;
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('##### Error Handler #####');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(PORT, () => console.log(`[server] Server started at http://localhost:${PORT}`));
